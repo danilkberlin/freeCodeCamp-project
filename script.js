7890d16acf125a221cb91edaec6caf59abeced6b
danilkberlin <danilkravets19@gmail.com>
2023-12-26 16:21:30 +0100

    function pick + for

--- script.js
+++ script.js
@@ -261,5 +261,12 @@ function pickEight() {
 }
 
 function pick(guess) {
+  let numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
 
+  }
 }
